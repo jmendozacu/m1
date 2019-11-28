667381eac7f068e0264c75e96f1098b4db0f5362
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-29 00:45:29 +0600

    no message

--- opcache.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-type: application/json');
-$a = opcache_get_status();
-unset($a['scripts']);
-echo json_encode($a, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
